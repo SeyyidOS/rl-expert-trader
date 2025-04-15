cbb816db0dc5af5f063fb3dd2d4308f76d2d1632
SeyyidOS <seyyidosmansevgili@gmail.com>
2025-04-16 00:01:24 +0300

    File add: Converted work with dict state space

--- src/utils/env_factory.py
+++ src/utils/env_factory.py
@@ -3,31 +3,52 @@ from typing import Dict
 import pandas as pd
 from stable_baselines3.common.monitor import Monitor
 from src.env.spot_trading_env import SpotTradingEnv
+from gymnasium.wrappers import FlattenObservation
 
 
-def make_train_env(data: Dict[str, pd.DataFrame]) -> Monitor:
+def make_train_env(data: Dict[str, pd.DataFrame], run_dir: str = None,
+                   monitor: bool = True, flatten: bool = True) -> Monitor:
     """
     Creates a monitored training environment using all assets except ETH.
     Args:
-        data (Dict[str, pd.DataFrame]): Market data.
+        :param run_dir:
+        :param data:
+        :param flatten:
+        :param monitor:
     Returns:
         Monitor: A monitored training environment.
+
     """
-    # Exclude ETH to diversify the training dataset.
-    dfs = [df for key, df in data.items() if key != "eth"]
-    env_instance = SpotTradingEnv(dfs, mode="train")
-    return Monitor(env_instance)
+    env_instance = SpotTradingEnv(data, mode="train", log_path=run_dir)
+
+    if monitor:
+        env_instance = Monitor(env_instance)
+
+    if flatten:
+        env_instance = FlattenObservation(env_instance)
+
+    return env_instance
 
 
-def make_eval_env(data: Dict[str, pd.DataFrame]) -> Monitor:
+def make_eval_env(data: Dict[str, pd.DataFrame], run_dir: str = None,
+                   monitor: bool = True, flatten: bool = True) -> Monitor:
     """
     Creates a monitored evaluation environment using a subset of ETH data.
     Args:
-        data (Dict[str, pd.DataFrame]): Market data.
+        :param run_dir:
+        :param data:
+        :param flatten:
+        :param monitor:
     Returns:
         Monitor: A monitored evaluation environment.
+
     """
-    eth_df = data["eth"]
-    # Use a subset (e.g., last 2400 data points) for evaluation.
-    env_instance = SpotTradingEnv([eth_df[-2400:]], mode="eval")
-    return Monitor(env_instance)
+    env_instance = SpotTradingEnv(data, mode="eval", log_path=run_dir)
+
+    if monitor:
+        env_instance = Monitor(env_instance)
+
+    if flatten:
+        env_instance = FlattenObservation(env_instance)
+
+    return env_instance
